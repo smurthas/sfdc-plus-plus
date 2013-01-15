ecd707e2ba23f5e86cb1a56f87410e0e37e8723e
Simon Murtha-Smith <simon@murtha-smith.com>
2013-01-14 18:45:26 -0800

    add create shortcuts, refactor to common functions

--- js/sfdc.js
+++ js/sfdc.js
@@ -13,30 +13,44 @@ Mousetrap.bind('o', function() {
   document.location = $(a).attr('href');
 });
 
+function gotoTab(tab) {
+  document.location = $('#' + tab + '_Tab').children().attr('href').slice(0, -2);
+}
+
+function createNew(type) {
+  document.location = $('#createNewMenu').find('.' + type + 'Mru').attr('href');
+}
+
 // g l → go to my leads
-Mousetrap.bind('g l', function() {
-  document.location = $('#Lead_Tab').children().attr('href').slice(0, -2);
-});
+Mousetrap.bind('g l', function() { gotoTab('Lead'); });
+
+// c l → create new lead
+Mousetrap.bind('c l', function() { createNew('lead'); });
 
 // g a → go to my accounts
-Mousetrap.bind('g a', function() {
-  document.location = $('#Account_Tab').children().attr('href').slice(0, -2);
-});
+Mousetrap.bind('g a', function() { gotoTab('Account'); });
+
+// c a → create new account
+Mousetrap.bind('c a', function() { createNew('account'); });
 
 // g c → go to my contacts
-Mousetrap.bind('g c', function() {
-  document.location = $('#Contact_Tab').children().attr('href').slice(0, -2);
-});
+Mousetrap.bind('g c', function() { gotoTab('Contact'); });
+
+// c c → create new contact
+Mousetrap.bind('c c', function() { createNew('contact'); });
 
 // g o → go to my opporunities
-Mousetrap.bind('g o', function() {
-  document.location = $('#Opportunity_Tab').children().attr('href').slice(0, -2);
-});
+Mousetrap.bind('g o', function() { gotoTab('Opportunity'); });
+
+// c o → create new opportunity
+Mousetrap.bind('c o', function() { createNew('opportunity'); });
 
 // g r → go to reports
-Mousetrap.bind('g r', function() {
-  document.location = $('#report_Tab').children().attr('href').slice(0, -2);
-});
+Mousetrap.bind('g r', function() { gotoTab('report'); });
+
+
+// c t → create new task
+Mousetrap.bind('c t', function() { createNew('task'); });
 
 var index = -1;
 Mousetrap.bind('j', function() {
@@ -58,3 +72,4 @@ Mousetrap.bind('k', function() {
   var a = $(row.find('table').find('td')[3]).find('a');
   a.focus();
 });
+
